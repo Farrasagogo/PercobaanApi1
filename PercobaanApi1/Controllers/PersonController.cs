c39b2fba8efebe774c127d9c9f37abe7c4d14a9a
Administrator <Administrator@WIN-CFITIP2EBFC>
2024-03-08 22:45:05 +0700

    Add project files.

---
+++ PercobaanApi1/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+namespace PercobaanApi1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("api/person/{id}")]
+        public ActionResult FilterId(int id)
+        {
+            PersonContext context = new PersonContext();
+            Person person = context.FilterId(id);
+            if (person != null)
+            {
+                return Ok(person);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
